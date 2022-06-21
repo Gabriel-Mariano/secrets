75e104dd3c931487b39671b2731493d40211d756
Gabriel Mariano Oliveira <gabrielmarianooliveira@MacBook-Pro-de-Gabriel.local>
2022-06-20 22:27:32 -0300

    feat: Instalação da lib de navegação entre as telas

---
+++ src/views/auth/signIn/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+const SignInScreen = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export { SignInScreen };
